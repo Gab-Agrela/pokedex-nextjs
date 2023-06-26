c8b09b4faa08c54791f929aeaba3a70dbd0bb765
Agrela <gabriel.agrela@hotmail.com>
2023-06-26 15:41:44 -0300

    feat: About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,16 @@
+import styles from "../styles/About.module.css";
+
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className={styles.container}>
+      <h1>Sobre</h1>
+      <p>
+        Essa é uma aplicação desenvolvida com Next.js. Como um toque especial,
+        adotei o tema da Pokédex, a enciclopédia digital que nos ajuda a
+        explorar o vasto mundo dos Pokémon. Com base na API do Pokémon, trago a
+        você informações detalhadas sobre centenas de espécies, seus tipos e
+        muito mais.
+      </p>
+    </div>
+  );
 }
